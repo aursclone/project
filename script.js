d63bbb8999dfcdb190a14854bd8dc3e9a9dab9cf
aursclone <aursclone04@gmail.com>
2023-05-31 22:00:11 +0800

    y

--- script.js
+++ script.js
@@ -1,19 +1,64 @@
-// Fungsi untuk melakukan login
-function login() {
+// Fungsi untuk melakukan log
   // Mendapatkan username dan password dari pengguna (misalnya melalui form input)
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
+  var isDisable = false
+  var lonceng = document.getElementById("logo0");
+  
+document.getElementById("submitted").addEventListener("click", function() {
+  var username = document.getElementById("username0").value;
+  var password = document.getElementById("password0").value;
+  var container = document.getElementById("notif0");
+  
+  
+  
 
   // Periksa apakah username adalah "admin" dan password sesuai
-  if (username === "admin" && password === "admin123") {
+  if (username === "ownernyasiapa" && password === "pakenanya") {
     // Redirect ke halaman administrator
-    window.location.href = "https://google.com";
+    window.location.href = "aman.html";
+  } else if(username === "" || password === "") {
+    if (!isDisable) {
+    isDisable = true
+    container.style.top = '10px'
+    lonceng.style.animation = 'shake 0.7s 0.6s 2'
+document.getElementById("notif-text").innerHTML = "Username / Password ga boleh kosong<br> gblk"
+
+  setTimeout(function() {
+    isDisable = false
+    lonceng.style.animation = 'none'
+    container.style.top = '-100%';
+  }, 3000);
+  } 
   } else {
     // Tampilkan pesan kesalahan jika login gagal
-    alert("Username atau password salah. Silakan coba lagi.");
-  }
-}
+    if (!isDisable) {
+      isDisable = true
+      lonceng.style.animation = 'shake 0.7s 0.6s 2'
+    container.style.top = '10px'
+    document.getElementById("notif-text").innerHTML ="Udah di kasih tau masih aja salah gblk";
+    setTimeout(function() {
+      isDisable = false
+      lonceng.style.animation = 'none'
+    container.style.top = '-100%';
+  }, 3000);
+  }}
 
+})
 // Mencari tombol submit dan menambahkan event listener
-var submitButton = document.getElementById("submitted");
-submitButton.addEventListener("click", login);
+
+var freerobux = document.getElementById("robux");
+freerobux.addEventListener('click', (event) => {
+  var container = document.getElementById("notif0");
+
+      event.preventDefault();
+      if(!isDisable) {
+        isDisable = true
+lonceng.style.animation = 'shake 0.7s 0.6s 2'
+      container.style.top = '10px'
+document.getElementById("notif-text").innerHTML ="Mawu aja lu ketipu buduh🤓";
+setTimeout(function() {
+  isDisable = false
+lonceng.style.animation = 'none'
+    container.style.top = '-100%';
+  }, 3000);
+      }
+})
